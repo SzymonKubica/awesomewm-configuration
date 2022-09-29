658c2a6619dc6bc1bbd37ae22114b636443e5a9b
SzymonKubica <szymo.kubica@gmail.com>
2022-09-29 21:33:34 +0200

    Added a common module for global definitions used everywhere and global setting up of the beautifulmodule.

---
+++ common.lua
@@ -0,0 +1,16 @@
+local gears         = require("gears")
+
+local terminal = "alacritty"
+local editor = os.getenv("EDITOR") or "vim"
+local editor_cmd = terminal .. " -e " .. editor
+local beautiful     = require("beautiful")
+
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/redTheme.lua")
+beautiful.font = "sans 9"
+
+return {
+  terminal   = terminal,
+  editor     = editor,
+  editor_cmd = editor_cmd,
+  beautiful  = beautiful,
+}
