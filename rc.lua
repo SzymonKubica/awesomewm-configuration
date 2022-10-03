48053cd5245a2c22276a03760074ec345915e6db
SzymonKubica <szymo.kubica@gmail.com>
2022-10-03 09:48:00 +0200

    Fixed code formatting and optimised imports.

--- rc.lua
+++ rc.lua
@@ -4,17 +4,16 @@ pcall(require, "luarocks.loader")
 
 -- AwesomeWM libraries
 local awful   = require("awful")
-local wibox   = require("wibox")
 local naughty = require("naughty")
 local menubar = require("menubar")
-
--- Common global definitions
-local common = require("common")
 
 require("awful.autofocus")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
+
+-- Common global definitions
+local common = require("common")
 
 -- Core Components
 local tasklist    = require("components.tasklist")
@@ -22,71 +21,63 @@ local taglist     = require("components.taglist")
 local wibox_template = require("components.custom_wibox").wibox_template
 local setup_wibox = require("components.custom_wibox").setup_wibox
 
+-- Utilities
+local autostart     = require("utilities.autostart")
+local set_wallpaper = require("utilities.theming").set_wallpaper
+local round_corners = require("utilities.theming").round_corners
+
 -- Keybindings
 local globalkeys    = require("keybindings.globalkeys")
 local clientkeys    = require("keybindings.clientkeys")
 local mousebuttons  = require("keybindings.mousebindings").mousebuttons
 local clientbuttons = require("keybindings.mousebindings").clientbuttons
 
--- Widgets
-local minimiser          = require("widgets.minimiser.minimiser")
-local cpu_widget         = require("widgets.cpu-widget.cpu-widget")
-local mytextclock        = wibox.widget.textclock("%d %b %H:%M")
-local language_widget    = require("widgets.keyboard-language-widget.keyboard-language-widget")
-local brightness_widget  = require("widgets.brightness-widget.brightness")
-local battery_arc_widget = require("widgets.batteryarc-widget.batteryarc")
-local menu_widget        = require("widgets.logout-menu-widget.logout-menu")
-local volume_widget      = require("widgets.volume-widget.volume")
-local separator          = wibox.widget.textbox(" ")
-
--- Utilities
-local autostart     = require("utilities.autostart")
-local set_wallpaper = require("utilities.theming").set_wallpaper
-local round_corners = require("utilities.theming").round_corners
+-- Set global keybindings.
+root.buttons(mousebuttons)
+root.keys(globalkeys)
 
 -- {{{ Error handling
 
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+  naughty.notify({
+    preset = naughty.config.presets.critical,
+    title = "Oops, there were errors during startup!",
+    text = awesome.startup_errors
+  })
 end
 
 -- Handle runtime errors after startup
 do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
+  local in_error = false
+  awesome.connect_signal("debug::error",
+    function (err)
+      -- Make sure we don't go into an endless error loop
+      if in_error then return end
+      in_error = true
 
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
+      naughty.notify({
+        preset = naughty.config.presets.critical,
+        title = "Oops, an error happened!",
+        text = tostring(err)
+      })
+
+      in_error = false
     end)
 end
 
 -- }}}
 
--- {{{ Variable definitions
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile,
-    awful.layout.suit.spiral.dwindle,
+  awful.layout.suit.tile,
+  awful.layout.suit.spiral.dwindle,
 }
 
--- {{{ Menu
-menubar.utils.terminal = common.terminal -- Set the terminal for applications that require it
--- }}}
-
--- {{{ Wibar
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
+-- Set the terminal for applications that require it
+menubar.utils.terminal = common.terminal
 
 awful.screen.connect_for_each_screen(function(s)
   set_wallpaper(s)
@@ -104,25 +95,23 @@ awful.screen.connect_for_each_screen(function(s)
   s.mywibox = wibox_template(s)
   setup_wibox(s)
 end)
--- }}}
 
--- Set global keybindings.
-root.buttons(mousebuttons)
-root.keys(globalkeys)
 
 -- {{{ Rules
+
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = common.beautiful.border_width,
-                     border_color = common.beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
+      properties = {
+        border_width = common.beautiful.border_width,
+        border_color = common.beautiful.border_normal,
+        focus        = awful.client.focus.filter,
+        raise        = true,
+        keys         = clientkeys,
+        buttons      = clientbuttons,
+        screen       = awful.screen.preferred,
+        placement    = awful.placement.no_overlap+awful.placement.no_offscreen
      }
     },
 
@@ -156,12 +145,15 @@ awful.rules.rules = {
           "ConfigManager",  -- Thunderbird's about:config.
           "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
         }
-      }, properties = { floating = true }},
-
+      },
+      properties = { floating = true }},
 }
 -- }}}
 
 -- {{{ Signals
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
 
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage",
