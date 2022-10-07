e1346981f551caeb4b1ed5b790c79c120acd56b0
SzymonKubica <szymo.kubica@gmail.com>
2022-10-07 07:32:52 +0100

    Updated the selection of the editor.

--- common.lua
+++ common.lua
@@ -1,7 +1,7 @@
 local gears         = require("gears")
 
 local terminal = "alacritty"
-local editor = os.getenv("EDITOR") or "vim"
+local editor = os.getenv("EDITOR") or "nvim"
 local editor_cmd = terminal .. " -e " .. editor
 local beautiful     = require("beautiful")
 
